34966a37d830bd166d9b87eb09d95b51fa44b0fa
Özgür <ozgur.gundogdu@gmail.com>
2017-06-15 01:30:52 +0300

    ...

--- tinder/src/containers/book-list.js
+++ tinder/src/containers/book-list.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import {connect} from "react-redux";
-
+import {selectBook} from "../actions/index";
+import {bindActionCreators} from "redux";
 
 class BookList extends Component {
     render() {
@@ -22,8 +23,14 @@ class BookList extends Component {
 // state içindeki propertyler içerde this.props.books şeklinde kullanılıyor
 function mapStateToProps(state) {
     return {
-      books: state.books
+        books: state.books
     };
 }
 
-export default connect(mapStateToProps)(BookList);
+function mapDispatchToProps(dispatch) {
+    // selectBook state ine selectBook actionCreator fonksiyonunu register ediyor.
+    // daha sonra ilgili reducer digest edip return ettiği objeyi selectBook state ine set etmiş olacak
+    return bindActionCreators({selectBook: selectBook}, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
